638547b68badf783d7e46c065809cfa7f73ccc10
arborealissolutions <95171836+arborealissolutions@users.noreply.github.com>
2023-04-15 01:05:06 -0400

    Update Resume.py

--- Resume.py
+++ Resume.py
@@ -72,17 +72,26 @@ contactInformation = [
 education = [
     'Miami University, Oxford OH',
     'Bachelor of Arts',
-    'Urban and Regional Planning, Sustainability, GISci Certificate',
 ]
 metaData = [
     'Created using Python, Reportlab',
     'For source code, go to GitHub:',
     'https://tinyurl.com/freundresume'
 ]
-latestJob = 'Database Administrator and Developer (Database Specialist)'
-latestEmployer = 'Hamilton County Public Health, Cincinnati OH'
-latestTime = 'November 2019 - Present'
+latestJob = 'Reporting and Data Science Analyst'
+latestEmployer = 'Independent Contractor, Fully Remote'
+latestTime = 'January 2023 - Present'
 latestDesc = [
+    'Created, updated, and maintained Stored Procedures in Microsoft SQL to ensure proper reporting and data analytics. Updated PowerBI interfaces and ',
+    'adjusted DAX and visual compontents. Utilized Windward reporting to generate reports for internal and external clients. Participated in SCRUM ',
+    'and utliized Sprints to ensure workflows and business logic were in place in a timely manner. Attened daily stand ups and one-on-ones with ',
+    'the data and reporting team. Created a database diagram to understand the system and how it interconnects as a late-arrival in a fast-paced ',
+    'start up environment.'
+]
+previousJob = 'Database Administrator and Developer (Database Specialist)'
+previousEmployer = 'Hamilton County Public Health, Cincinnati OH'
+previousTime = 'November 2019 - January 2023'
+previousDesc = [
     'Responsible for creating, maintaining, and updating production level relational databases (Microsoft SQL, '
     'POSTGRESQL, and Microsoft Access ',
     'databases for agency-wide programs and projects. Utilized SQL, Python, PowerShell, VBA, and C# to '
@@ -91,10 +100,10 @@ latestDesc = [
     'through ArcPY, both locally and on ',
     'the web. Developed various data cleaning and verification tools to improve reliability of in agency-wide tools.'
 ]
-previousJob = 'GIS Specialist (Public Health Sanitarian I)'
-previousEmployer = 'Hamilton County Public Health, Cincinnati OH'
-previousTime = 'October 2018 - November 2019'
-previousDesc = [
+job1 = 'GIS Specialist (Public Health Sanitarian I)'
+employer1 = 'Hamilton County Public Health, Cincinnati OH'
+time1 = 'October 2018 - November 2019'
+desc1 = [
     'Developed and maintained department GIS data, including managing, updating, and processing data for hosting on '
     'ArcGIS Online. Utilized Python ',
     'scripts to manage routine updates, query non-GIS data, and join existing layers with databases. Represented '
@@ -103,34 +112,34 @@ previousDesc = [
     'applications tied to existing ESRI products.',
     'Followed and applied State code during routine inspections on septic systems',
 ]
-job1 = 'AmeriCorps VISTA Member'
-employer1 = 'Calhoun County Land Bank Authority, Marshall MI'
-time1 = 'July 2017 - July 2018'
-desc1 = [
+job2 = 'AmeriCorps VISTA Member'
+employer2 = 'Calhoun County Land Bank Authority, Marshall MI'
+time2 = 'July 2017 - July 2018'
+desc2 = [
     'Built capacity at host organization, which included streamlining program review processes, '
     'grant funded project tracking, and creating ',
     'a Neighborhood Health Index for the City of Albion through the data collection and cleaning followed by the '
     'application of ArcGIS methodologies',
 ]
-job2 = 'GIS Associate'
-employer2 = 'Miami University, Oxford OH (Contract with Butler County Board of Developmental Disabilities)'
-time2 = 'February 2017 - May 2017'
-desc2 = 'Responsible for repopulating and verifying data in databases and ArcGIS layers, in addition to generating ' \
+job3 = 'GIS Associate'
+employer3 = 'Miami University, Oxford OH (Contract with Butler County Board of Developmental Disabilities)'
+time3 = 'February 2017 - May 2017'
+desc3 = 'Responsible for repopulating and verifying data in databases and ArcGIS layers, in addition to generating ' \
         'more in-depth and functional maps utilizing ArcGIS'
-job3 = 'Researcher'
-employer3 = 'Miami University, Oxford OH'
-time3 = 'February 2016 - May 2017'
-desc3 = [
+job4 = 'Researcher'
+employer4 = 'Miami University, Oxford OH'
+time4 = 'February 2016 - May 2017'
+desc4 = [
     'Began research on ecosystem service provision in exurban parcels containing at least one pond, including '
     'creating a raster land use map and training ',
     'remote sensing analysis, identifying uses and desires of individuals living in isolated rural parcels that lead '
     'to publication ',
     '(https://www.mdpi.com/2073-445X/10/5/448)',
 ]
-job4 = 'GIS Associate'
-employer4 = 'Miami University, Oxford OH (Contract with Miami University Police Department, Parking Services)'
-time4 = 'February 2015 - May 2015'
-desc4 = 'Assisting in creating a web map, gathering data, and digitizing parking locations, both individually and in ' \
+job5 = 'GIS Associate'
+employer5 = 'Miami University, Oxford OH (Contract with Miami University Police Department, Parking Services)'
+time5 = 'February 2015 - May 2015'
+desc5 = 'Assisting in creating a web map, gathering data, and digitizing parking locations, both individually and in ' \
         'a team, required time management skills'
 skillHeader1 = 'Technology and Communication'
 skillSet1 = [
@@ -146,7 +155,9 @@ skillSet1 = [
     'applications and functions',
     'Produced various processes and applications that allowed staff to interact with various data sources '
     'at a production level',
-    'Moved manual services into automated processes that freed up staff time and increased reliability'
+    'Moved manual services into automated processes that freed up staff time and increased reliability',
+    'Developed, updated, and tested Stored Procedures for various purposes; reporting, platform widgets, and other analytic functions',
+    'Leveraged PowerBI to display business data and to drill into summary data from platform widgets; providing an in-depth look at the data on request'
 ]
 skillHeader2 = 'Geospatial Analysis (ArcGIS 10.X, ENVI 5.X, ArcPY)'
 skillSet2 = [
@@ -162,7 +173,7 @@ skillSet2 = [
     'Used widgets in ArcGIS Online to improve the quality and effectiveness of programs within the organization',
     'Utilized the ArcPY framework to create various data cleaning, preparation, and management processes to ensure '
     'datasets used by staff',
-    'and public were accurate and up-to-date',
+    'and public were up-to-date',
     'Generated weekly reports, including heat maps, of COVID-19 cases in Hamilton County, OH',
 ]
 skillHeader3 = 'Research and Analysis'
@@ -186,6 +197,7 @@ skillSet4 = [
     'Worked on a continuous quality improvement project within organization',
     'Implemented improvements based on feedback from leadership and users based on their usage of tools and '
     'functions',
+    'Participatated in SCRUM and AGILE workflows'
 ]
 skillHeader5 = ''
 skillSet5 = [
@@ -213,7 +225,7 @@ create_features('Line', '', 0, 50, 550, 50)
 create_features('String', 'Education', 40, 748,
                 font_type=fontTypeBold, font_size=14)
 # Education String
-create_features('Multi', education, 120, 758,
+create_features('Multi', education, 110, 754,
                 font_type=fontTypeBase, font_size=textSize)
 
 # Separator Line between Education and Experience
@@ -223,75 +235,75 @@ create_features('Multi', metaData, 405, 758,
                 font_type=fontTypeBase, font_size=textSize)
 
 # Experience Header
-create_features('String', 'Experience', 40, 718,
+create_features('String', 'Experience', 40, 714,
                 font_type=fontTypeBold, font_size=14)
 
 # Current Job
-create_features('String', latestJob, 40, 706,
+create_features('String', latestJob, 40, 700,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('String', latestEmployer, 40, 694,
+create_features('String', latestEmployer, 40, 688,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('String', latestTime, 325, 700,
+create_features('String', latestTime, 325, 695,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('Multi', latestDesc, 50, 682,
+create_features('Multi', latestDesc, 50, 678,
                 font_type=fontTypeBase, font_size=textSize)
 
 # Previous Job
-create_features('String', previousJob, 40, 640,
+create_features('String', previousJob, 40, 620,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('String', previousEmployer, 40, 628,
+create_features('String', previousEmployer, 40, 608,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('String', previousTime, 325, 635,
+create_features('String', previousTime, 325, 615,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('Multi', previousDesc, 50, 616,
+create_features('Multi', previousDesc, 50, 596,
                 font_type=fontTypeBase, font_size=textSize)
 
 # Job 1
-create_features('String', job1, 40, 570,
+create_features('String', job1, 40, 550,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('String', employer1, 40, 558,
+create_features('String', employer1, 40, 538,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('String', time1, 325, 565,
+create_features('String', time1, 325, 545,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('Multi', desc1, 50, 546,
+create_features('Multi', desc1, 50, 526,
                 font_type=fontTypeBase, font_size=textSize)
 
-# Job 3
-create_features('String', job3, 40, 520,
-                font_type=fontTypeBase, font_size=headerSize)
-create_features('String', employer3, 40, 508,
-                font_type=fontTypeBase, font_size=headerSize)
-create_features('String', time3, 325, 515,
-                font_type=fontTypeBase, font_size=headerSize)
-create_features('Multi', desc3, 50, 496,
-                font_type=fontTypeBase, font_size=textSize)
+# Job 4
+# create_features('String', job4, 40, 520,
+#                 font_type=fontTypeBase, font_size=headerSize)
+# create_features('String', employer4, 40, 508,
+#                 font_type=fontTypeBase, font_size=headerSize)
+# create_features('String', time4, 325, 515,
+#                 font_type=fontTypeBase, font_size=headerSize)
+# create_features('Multi', desc4, 50, 496,
+#                 font_type=fontTypeBase, font_size=textSize)
 
 # Skills and Qualifications Header
 create_features('String', 'Skills and Qualifications', 40,
-                462, font_type=fontTypeBold, font_size=14)
+                478, font_type=fontTypeBold, font_size=14)
 
 # Technology and Communication
-create_features('String', skillHeader1, 40, 450,
+create_features('String', skillHeader1, 40, 462,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('Multi', skillSet1, 50, 438,
+create_features('Multi', skillSet1, 50, 450,
                 font_type=fontTypeBase, font_size=textSize)
 
 # GIS
-create_features('String', skillHeader2, 40, 355,
+create_features('String', skillHeader2, 40, 350,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('Multi', skillSet2, 50, 345,
+create_features('Multi', skillSet2, 50, 338,
                 font_type=fontTypeBase, font_size=textSize)
 
 # Research and Analysis
-create_features('String', skillHeader3, 40, 235,
+create_features('String', skillHeader3, 40, 230,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('Multi', skillSet3, 50, 220,
+create_features('Multi', skillSet3, 50, 218,
                 font_type=fontTypeBase, font_size=textSize)
 
 # Leadership, Collaboration, and Teamwork
-create_features('String', skillHeader4, 40, 150,
+create_features('String', skillHeader4, 40, 146,
                 font_type=fontTypeBase, font_size=headerSize)
-create_features('Multi', skillSet4, 50, 138,
+create_features('Multi', skillSet4, 50, 134,
                 font_type=fontTypeBase, font_size=textSize)
 
 # save the pdf
